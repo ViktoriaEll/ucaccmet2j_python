d1e4799a48cd025589670b7b30a2fe76326ee321
Viktoria Ellmies <v.ellmies@students.uu.nl>
2023-01-17 14:19:49 +0100

    Updated part 1 since I did it wrong the first time

--- weather_assignment.py
+++ weather_assignment.py
@@ -1,20 +1,29 @@
 # WEATHER ASSIGNMENT
 # Q 1.1
-
 import json
 with open('precipitation.json', encoding='utf-8') as file:
   precipitation_data = json.load(file)
 
+from csv import DictReader
+with open('stations.csv') as file:
+  reader = DictReader(file)
+  stations = list(reader)
 
+station_data = {}
 
+# Creating station dictionary
+for station in stations:
+   station_data[station['Location']] = station
+
+# Making the data a list of integers [year, month, day]
 for precipitation in precipitation_data:
     precipitation['date'] = (precipitation['date'].split('-'))
     for i in range(3):
         precipitation['date'][i] = int(precipitation['date'][i])
-#print(precipitation_data)
 
 total_monthly_precipitation = []
 
+# Calculating total monthly precipitation for Seattle
 for month in range(1,13):
     monthly_precipitation = 0
     for precipitation in precipitation_data:
@@ -24,7 +33,11 @@ for month in range(1,13):
     total_monthly_precipitation.append(monthly_precipitation)
 
 print(total_monthly_precipitation)
+station_data['Seattle']['total_monthly_precipitation'] = total_monthly_precipitation
 
-#with open('results.json', 'w', encoding='utf-8') as file:
- #   json.dump(total_monthly_precipitation, file, indent=4)
+print(station_data)
 
+with open('results.json', 'w', encoding='utf-8') as file:
+   json.dump(station_data, file, indent=4)
+
+# results.json also includes some information for the other stations which in order to make the graph using plot_results.py has to be removed
